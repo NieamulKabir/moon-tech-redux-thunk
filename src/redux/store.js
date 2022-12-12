60c9a87840b3c05367c3d78e690c3d813074dca5
NieamulKabir <kabir000079@gmail.com>
2022-12-12 23:00:06 +0600

    all updt

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import rootReducer from "./reducers/rootReducer";
+import logger from "redux-logger";
+import cartCounter from "./middlewares/cartCounter";
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(cartCounter, thunk, logger)));
+
+export default store;
